cfc0999e53b15998dfcfec62770e105b437af033
dell <ngo.trung.kien@sun-asterisk.com>
2022-03-07 10:29:15 +0700

    Chapter 11

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,7 +8,7 @@ module SessionsHelper
       @current_user ||= User.find_by id: user_id
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by id: user_id
-      if user&.authenticated?(cookies[:remember_token])
+      if user&.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -23,6 +23,12 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def login_remember user
+    log_in user
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+    redirect_back_or user
   end
 
   def remember user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  before_save :email_downcase
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true,
     length: {maximum: Settings.settings.user.username.max_length}
   validates :email, presence: true,
@@ -10,12 +12,10 @@ class User < ApplicationRecord
   validates :password, presence: true,
     length: {minimum: Settings.settings.user.password.min_length},
     allow_nil: true
-  private
-  def email_downcase
-    self.email = email.downcase
-  end
 
-  attr_accessor :remember_token
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 
   class << self
     def digest string
@@ -37,13 +37,28 @@ class User < ApplicationRecord
     update_column :remember_digest, User.digest(remember_token)
   end
 
-  def authenticated? remember_token
-    return false unless remember_digest
-
-    BCrypt::Password.new(remember_digest).is_password? remember_token
-  end
-
   def forget
     update_column :remember_digest, nil
+  end
+
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false unless digest
+
+    BCrypt::Password.new(digest).is_password? token
+  end
+
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+
+  private
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
