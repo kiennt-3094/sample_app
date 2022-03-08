7af173786213c11a4838319918666d0838159c88
dell <ngo.trung.kien@sun-asterisk.com>
2022-03-08 16:40:42 +0700

    Chapter 13

---
+++ app/models/micropost.rb
@@ -0,0 +1,13 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  scope :recent_posts, ->{order created_at: :desc}
+  has_one_attached :image
+  validates :content, presence: true,
+             length: {maximum: Settings.settings.micropost.content.max_length}
+  validates :image, content_type: {in: Settings.settings.image.type, message: :invalid_format},
+             size: {less_than: Settings.settings.image.max_size.megabytes, message: :oversized}
+  def display_image
+    image.variant resize_to_limit: [Settings.settings.image.limit,
+                                    Settings.settings.image.limit]
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -60,6 +61,10 @@ class User < ApplicationRecord
     reset_sent_at < Settings.settings.expiration_time.hours.ago
   end
 
+  def feed
+    microposts.newest
+  end
+
   private
   def downcase_email
     email.downcase!
@@ -72,6 +77,7 @@ class User < ApplicationRecord
 
   def create_reset_digest
     self.reset_token = User.new_token
-    update_columns reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
+    update_columns reset_digest: User.digest(reset_token),
+                   reset_sent_at: Time.zone.now
   end
 end
